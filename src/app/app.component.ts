181d994e3023374d2256d0e7e4d9ce73d5241fe5
Janio Isacura <janioisacura@gmail.com>
2021-04-16 12:38:34 -0300

    Instalación e implementación de Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  @Input()
+  color: ThemePalette;
   title = 'mapas';
 }
